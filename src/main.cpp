5634f54a2d6a59909b6527a6f15ff1e7aec3d3d2
Oğuz Toraman <oguz.toraman@protonmail.com>
2021-03-10 22:38:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,21 @@
  * 
  * PiCar control library C++ main file.
  * 
- * 
+ * Copyright (C) 2021 Oğuz Toraman <oguz.toraman@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 #include <iostream>
