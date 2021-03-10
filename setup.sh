53535044a9bb1c727c673b769b6022362f186ccd
Oğuz Toraman <oguz.toraman@protonmail.com>
2021-03-10 21:54:50 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+#   Setup.sh
+#   
+#   A bash script that updates the system and installs necessary software packages for PiCar
+#   
+echo "Updating the system..."
+sudo apt update
+sudo apt upgrade -y
+echo "Installing necessary softwares..."
+sudo apt install -y g++ libopencv-dev make wiringpi
+echo "Setup finished."
