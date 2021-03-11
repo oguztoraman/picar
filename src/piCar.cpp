de88ee7d017fc1a90eac6d28eb1946f52d48093b
Oğuz Toraman <oguz.toraman@protonmail.com>
2021-03-11 19:14:29 +0300

    Update piCar.cpp

--- src/piCar.cpp
+++ src/piCar.cpp
@@ -145,81 +145,81 @@ void PiCar::turn_camera(int direction, float degree)
 		if (degree < 4.5){
 			set_horizontal_servo_to_position(14);	/*	~0 degree	*/
 		} else if (degree >= 4.5 && degree < 13.5){
-			set_horizontal_servo_to_position(13);	/*	~9 degree	*/
+			set_horizontal_servo_to_position(13);	/*	~9 degrees	*/
 		} else if (degree >= 13.5 && degree < 22.5){
-			set_horizontal_servo_to_position(12);	/*	~18 degree	*/
+			set_horizontal_servo_to_position(12);	/*	~18 degrees	*/
 		} else if (degree >= 22.5 && degree < 31.5){
-			set_horizontal_servo_to_position(11);	/*	~27 degree	*/
+			set_horizontal_servo_to_position(11);	/*	~27 degrees	*/
 		} else if (degree >= 31.5 && degree < 40.5){
-			set_horizontal_servo_to_position(10);	/*	~36 degree	*/
+			set_horizontal_servo_to_position(10);	/*	~36 degrees	*/
 		} else if (degree >= 40.5 && degree < 49.5){
-			set_horizontal_servo_to_position(9);	/*	~45 degree	*/
+			set_horizontal_servo_to_position(9);	/*	~45 degrees	*/
 		} else if (degree >= 49.5 && degree < 58.5){
-			set_horizontal_servo_to_position(8);	/*	~54 degree	*/
+			set_horizontal_servo_to_position(8);	/*	~54 degrees	*/
 		} else if (degree >= 58.5 && degree < 67.5){
-			set_horizontal_servo_to_position(7);	/*	~63 degree	*/
+			set_horizontal_servo_to_position(7);	/*	~63 degrees	*/
 		} else if (degree >= 67.5 && degree < 76.5){
-			set_horizontal_servo_to_position(6);	/*	~72 degree	*/
+			set_horizontal_servo_to_position(6);	/*	~72 degrees	*/
 		} else if (degree >= 76.5 && degree < 85.5){
-			set_horizontal_servo_to_position(5);	/*	~81 degree	*/
+			set_horizontal_servo_to_position(5);	/*	~81 degrees	*/
 		} else if (degree >= 85.5){
-			set_horizontal_servo_to_position(4);	/*	~90 degree	*/
+			set_horizontal_servo_to_position(4);	/*	~90 degrees	*/
 		}
 	} else if (direction == LEFT) {
 		if (degree < 5){
 			set_horizontal_servo_to_position(14);	/*	~0 degree	*/
 		} else if (degree >= 5 && degree < 15){
-			set_horizontal_servo_to_position(15);	/*	~10 degree	*/
+			set_horizontal_servo_to_position(15);	/*	~10 degrees	*/
 		} else if (degree >= 15 && degree < 25){
-			set_horizontal_servo_to_position(16);	/*	~20 degree	*/
+			set_horizontal_servo_to_position(16);	/*	~20 degrees	*/
 		} else if (degree >= 25 && degree < 35){
-			set_horizontal_servo_to_position(17);	/*	~30 degree	*/
+			set_horizontal_servo_to_position(17);	/*	~30 degrees	*/
 		} else if (degree >= 35 && degree < 45){
-			set_horizontal_servo_to_position(18);	/*	~40 degree	*/
+			set_horizontal_servo_to_position(18);	/*	~40 degrees	*/
 		} else if (degree >= 45 && degree < 55){
-			set_horizontal_servo_to_position(19);	/*	~50 degree	*/
+			set_horizontal_servo_to_position(19);	/*	~50 degrees	*/
 		} else if (degree >= 55 && degree < 65){
-			set_horizontal_servo_to_position(20);	/*	~60 degree	*/
+			set_horizontal_servo_to_position(20);	/*	~60 degrees	*/
 		} else if (degree >= 65 && degree < 75){
-			set_horizontal_servo_to_position(21);	/*	~70 degree	*/
+			set_horizontal_servo_to_position(21);	/*	~70 degrees	*/
 		} else if (degree >= 75 && degree < 85){
-			set_horizontal_servo_to_position(22);	/*	~80 degree	*/
+			set_horizontal_servo_to_position(22);	/*	~80 degrees	*/
 		} else if (degree >= 85){
-			set_horizontal_servo_to_position(23);	/*	~90 degree	*/
+			set_horizontal_servo_to_position(23);	/*	~90 degrees	*/
 		}
 	} else if (direction == UP){
 		if (degree < 5){
-			set_vertical_servo_to_position(15);		/*	~0 degree	*/
+			set_vertical_servo_to_position(15);	/*	~0 degrees	*/
 		} else if (degree >= 5 && degree < 15){
-			set_vertical_servo_to_position(14);		/*	~10 degree	*/
+			set_vertical_servo_to_position(14);	/*	~10 degrees	*/
 		} else if (degree >= 15 && degree < 25){
-			set_vertical_servo_to_position(13);		/*	~20 degree	*/
+			set_vertical_servo_to_position(13);	/*	~20 degrees	*/
 		} else if (degree >= 25 && degree < 35){
-			set_vertical_servo_to_position(12);		/*	~30 degree	*/
+			set_vertical_servo_to_position(12);	/*	~30 degrees	*/
 		} else if (degree >= 35 && degree < 45){
-			set_vertical_servo_to_position(11);		/*	~40 degree	*/
+			set_vertical_servo_to_position(11);	/*	~40 degrees	*/
 		} else if (degree >= 45 && degree < 55){
-			set_vertical_servo_to_position(10);		/*	~50 degree	*/
+			set_vertical_servo_to_position(10);	/*	~50 degrees	*/
 		} else if (degree >= 55 && degree < 65){
-			set_vertical_servo_to_position(9);		/*	~60 degree	*/
+			set_vertical_servo_to_position(9);	/*	~60 degrees	*/
 		} else if (degree >= 65 && degree < 75){
-			set_vertical_servo_to_position(8);		/*	~70 degree	*/
+			set_vertical_servo_to_position(8);	/*	~70 degrees	*/
 		} else if (degree >= 75 && degree < 85){
-			set_vertical_servo_to_position(7);		/*	~80 degree	*/
+			set_vertical_servo_to_position(7);	/*	~80 degrees	*/
 		}  else if (degree >= 85){
-			set_vertical_servo_to_position(6);		/*	~90 degree	*/
+			set_vertical_servo_to_position(6);	/*	~90 degrees	*/
 		}
 	} else if (direction == DOWN){
 		if (degree < 3.75){
-			set_vertical_servo_to_position(15);		/*	~0 degree	*/
+			set_vertical_servo_to_position(15);	/*	~0 degree	*/
 		} else if (degree >= 3.57 && degree < 11.25){
-			set_vertical_servo_to_position(16);		/*	~7.5 degree	*/
+			set_vertical_servo_to_position(16);	/*	~7.5 degrees	*/
 		} else if (degree >= 11.25 && degree < 18.75){
-			set_vertical_servo_to_position(17);		/*	~15 degree	*/
+			set_vertical_servo_to_position(17);	/*	~15 degrees	*/
 		} else if (degree >= 18.75 && degree < 22.25){
-			set_vertical_servo_to_position(18);		/*	~22.5 degree*/
+			set_vertical_servo_to_position(18);	/*	~22.5 degrees   */
 		}  else if (degree >= 22.25){
-			set_vertical_servo_to_position(19);		/*	~30 degree	*/
+			set_vertical_servo_to_position(19);	/*	~30 degrees	*/
 		}
 	} else {
 		std::cout << "Invalid direction!" << std::endl;
